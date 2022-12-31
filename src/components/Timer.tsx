07c83c9668790476ccf7c28bf37c8cedb0fd50f2
Lapor Chen <31620109+laporchen@users.noreply.github.com>
2022-12-31 19:20:15 +0800

    feat: add timer, animation to words

---
+++ src/components/Timer.tsx
@@ -0,0 +1,60 @@
+import {useState, useEffect, useImperativeHandle} from "react"
+import React from "react"
+import {Text} from "@chakra-ui/react"
+import "./Timer.css"
+export interface TimerPropsType {
+	startTimer: () => void
+	endTimer: () => void
+	resetTimer: () => void
+}
+const Timer = React.forwardRef((_, ref: React.Ref<TimerPropsType>) => {
+	const [startTime, setStartTime] = useState<number | null>(null)
+	const [endTime, setEndTime] = useState<number | null>(null)
+	const [isStarted, setStarted] = useState(false)
+	function startTimer() {
+		setStartTime(Date.now())
+		setEndTime(null)
+		setStarted(true)
+	}
+	function endTimer() {
+		setEndTime(Date.now())
+		setStarted(false)
+	}
+	function resetTimer() {
+		setStartTime(null)
+	}
+	useImperativeHandle(ref, () => {
+		return {
+			startTimer: startTimer,
+			endTimer: endTimer,
+			resetTimer: resetTimer
+		}
+	})
+
+	useEffect(() => {
+		let interval: number | null = 0
+		if (isStarted) interval = setInterval(() => setEndTime(Date.now()), 50)
+		return () => {
+			if (isStarted) clearInterval(interval!)
+		}
+	})
+	let timer = ""
+	if (startTime) {
+		const currentTime = new Date((endTime ? endTime : Date.now()) - startTime)
+		const hours = currentTime.getHours() - 8
+		const minutes = currentTime.getMinutes()
+		const seconds = currentTime.getSeconds()
+		const milliSeconds = Math.floor(currentTime.getMilliseconds() / 10)
+		if (hours) timer += hours.toString() + ":"
+		if (hours || minutes) timer += minutes.toString() + ":"
+		timer += seconds.toString() + "."
+		timer += milliSeconds.toString()
+	}
+	return (
+		<Text className={"timer"} fontSize="60px">
+			{timer}
+		</Text>
+	)
+})
+
+export default Timer
--- src/components/typing.tsx
+++ src/components/typing.tsx
@@ -1,14 +1,28 @@
-import {Center, Box, HStack, Text} from "@chakra-ui/react"
-import {useEffect, useState} from "react"
+import {Center, Text} from "@chakra-ui/react"
+import React, {useEffect, useRef, useState} from "react"
 import randomWords from "random-words"
+import Timer from "./Timer"
+import {TimerPropsType} from "./Timer"
 import "./typing.css"
 
+
 export default function Typing() {
-	const [wordList, setWordList] = useState(randomWords(5))
+	const wordCount = 20
+	const [wordList, setWordList] = useState(randomWords(wordCount))
 	const [targetIndex, setTargetIndex] = useState(0)
-	const [userInputList, setUserInputList] = useState(Array.from({length: wordList.length}, () => ""))
+	const [userInputList, setUserInputList] = useState(Array.from({length: wordCount}, () => ""))
 	const [userInputIndex, setUserInputIndex] = useState(0)
+	const [hasStartTyping, setStartTyping] = useState(false)
+	const timerRef = React.createRef<TimerPropsType>()
 	useEffect(() => {
+		function reset() {
+			timerRef.current?.resetTimer()
+			setStartTyping(false)
+			setTargetIndex(0)
+			setUserInputIndex(0)
+			setWordList(randomWords(wordCount))
+			setUserInputList(Array.from({length: wordList.length}, () => ""))
+		}
 		function appendWord(e: KeyboardEvent) {
 			if (isAlphanum(e.key)) {
 				let inputs = userInputList.slice()
@@ -17,7 +31,14 @@ export default function Typing() {
 			}
 		}
 		function modifyWord(e: KeyboardEvent) {
-			if (e.keyCode === 8) {
+			if (!hasStartTyping) {
+				if (targetIndex + 1 === wordList.length) {
+					reset()
+				}
+				setStartTyping(true)
+				timerRef.current?.startTimer()
+			}
+			if (e.key === "Backspace") {
 				let inputs = userInputList.slice()
 				if (inputs[userInputIndex].length === 0) {
 					setUserInputIndex(Math.max(0, userInputIndex - 1))
@@ -29,13 +50,10 @@ export default function Typing() {
 				return
 			}
 
-			if (e.keyCode === 32) {
+			if (e.code === "Space") {
 				if (targetIndex + 1 === wordList.length) {
-					alert("Done")
-					setTargetIndex(0)
-					setUserInputIndex(0)
-					setWordList(randomWords(5))
-					setUserInputList(Array.from({length: wordList.length}, () => ""))
+					timerRef.current?.endTimer()
+					setStartTyping(false)
 					return
 				}
 				setTargetIndex((targetIndex + 1) % wordList.length)
@@ -51,9 +69,6 @@ export default function Typing() {
 		}
 	})
 	function currentWordHandle(wordIndex: number) {
-		const wordEnd =
-			wordList[wordIndex].length === userInputList[userInputIndex].length && wordIndex === userInputIndex
-				? "currentLetter" : ""
 		return (
 			wordList[wordIndex].split("").map((key, index) => {
 				let className = ""
@@ -76,23 +91,47 @@ export default function Typing() {
 				}
 
 				return (
-					<a className={className}>{key}</a>
+					<a key={index} className={className}>{key}</a>
 				)
-			}).concat([<a className={wordEnd}>&nbsp;</a>])
+			})
 		)
 	}
-	const wordsDiv = wordList.map((_, index) => {
-		return (<div key={index}>
-			<Text fontSize={"3xl"}>
-				{(currentWordHandle(index))}
-			</Text>
-		</div>)
+	let pastGridItem: JSX.Element[] = []
+	let laterGridItem: JSX.Element[] = []
+	let currentGridItem: JSX.Element[] = []
+	wordList.forEach((_, index) => {
+		let fontSize = index === userInputIndex ? "150px" : "40px"
+		let currentWordAnimation = index === userInputIndex ? "currentWordMoveIn" : ""
+		let wordItem = (<Text key={index} fontSize={fontSize} className={currentWordAnimation}> {(currentWordHandle(index))} </Text>)
+		if (index < userInputIndex) {
+			pastGridItem.push(wordItem)
+			return
+		}
+		if (index === userInputIndex) {
+			currentGridItem.push(wordItem)
+			return
+		}
+		laterGridItem.push(wordItem)
+
 	})
 	return (
-		<div>
-			<Center width={"100%"} height="100%">
-				{wordsDiv}
+		<div className="main">
+			<div className="pastWords">
+				<Center gap={"12px"}>
+					{pastGridItem}
+				</Center>
+			</div>
+			<div className="laterWord">
+				<Center gap={"12px"}>
+					{laterGridItem}
+				</Center>
+			</div>
+			<Center width={"100%"} height="800px">
+				<div className="currentWord">
+					{currentGridItem}
+				</div>
 			</Center>
+			<Timer ref={timerRef} />
 		</div>
 	)
 }
